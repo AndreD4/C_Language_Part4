36a3af39daeccee2ce1de6b20e85a8ae7bf16d6e
AndreD4 <andremai629@gmail.com>
2023-10-03 16:52:08 +0900

    Vector basic example

---
+++ Vectors/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector> // dont forget this in order to use vector
+
+using namespace std;
+
+int main() 
+{
+//    vector <char> vowels;      // empty
+//    vector <char> vowels (5);  // 5 initialized to zero
+    vector <char> vowels {'a','e','i','o','u'};
+    
+    
+    cout << vowels[0] << endl;
+    cout << vowels[4] << endl;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    cout << endl;
+    return 0;    
+}
