c575a5f0f3b9ee9ff7600f48c5638bdd6464a625
AndreD4 <andremai629@gmail.com>
2023-10-03 15:29:40 +0900

    Example of assignment statement

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -10,7 +10,13 @@ int main()
     cout <<"The last vowel is: " << vowels[4] << endl;
     
 //    cin >> vowels[5]; out of bounds - dont do this
+
+    double hi_temps[] {90.1, 89.8, 77.5, 81.6};
+    cout << "\nThe first high temperture is: " << hi_temps[0] << endl;
     
+    hi_temps[0] = 100.7;   // set the first element in hi_temps to 100.7
+    
+    cout << "The first high temperture is now: " << hi_temps[0] << endl;
     
     
     
