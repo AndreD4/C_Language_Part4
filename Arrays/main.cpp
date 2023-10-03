9bdcc470263107cc54cc6369479da7fbe874d789
AndreD4 <andremai629@gmail.com>
2023-10-03 15:52:48 +0900

    Storing into arrays

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -20,6 +20,30 @@ int main()
     
     
     
+    int test_scores[5] {100, 90, 80, 90, 60};
+    
+    cout << "\nFirst score at index 0: " << test_scores [0] << endl;
+    cout << "Second score at index 1: " << test_scores [1] << endl;
+    cout << "Third score at index 2: " << test_scores [2] << endl;
+    cout << "Fourth score at index 3: " << test_scores [3] << endl;
+    cout << "Fith score at index 4: " << test_scores [4] << endl;
+    
+    cout << "\nEnter 5 test scores: ";
+    cin >> test_scores[0];
+    cin >> test_scores[1];
+    cin >> test_scores[2];
+    cin >> test_scores[3];
+    cin >> test_scores[4];
+    
+    cout << "\nThe updated array is:" << endl;
+    cout << "\nFirst score at index 0: " << test_scores [0] << endl;
+    cout << "Second score at index 1: " << test_scores [1] << endl;
+    cout << "Third score at index 2: " << test_scores [2] << endl;
+    cout << "Fourth score at index 3: " << test_scores [3] << endl;
+    cout << "Fith score at index 4: " << test_scores [4] << endl;
+    
+    
+    
     
     
     
