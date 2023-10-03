e4b3d8f152feae3429ee14b0472d493c2733de04
AndreD4 <andremai629@gmail.com>
2023-10-03 17:03:19 +0900

    array syntax & vector syntax

--- Vectors/main.cpp
+++ Vectors/main.cpp
@@ -13,8 +13,23 @@ int main()
     cout << vowels[0] << endl;
     cout << vowels[4] << endl;
     
+//    vector <int> text_scores (3); // 3 elements all initialized to zero
+//    vertor <int>  text_scores (3,100); // 3 elements all initialized to 100
+
+    vector <int> test_scores {100, 98, 89};
     
+    cout << "\nTest score uing array syntax:" << endl;
+    cout << test_scores[0] << endl;
+    cout << test_scores[1] << endl;
+    cout << test_scores[2] << endl;
     
+    cout << "\nTest scores using vector syntax:" << endl;
+    
+    cout << test_scores.at(0) << endl;
+    cout << test_scores.at(1) << endl;
+    cout << test_scores.at(2) << endl;
+    cout << "\nThere are " << test_scores.size() << " score in the vector" << endl; 
+   
     
     
     
