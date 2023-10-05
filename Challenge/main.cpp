c1529658f8dfc9589e6990b325aa87806e9ae5bc
AndreD4 <andremai629@gmail.com>
2023-10-05 15:33:48 +0900

    Challenge

---
+++ Challenge/main.cpp
@@ -0,0 +1,26 @@
+/*
+
+    Write program as followed
+     
+    Declare 2 empty vectors of integers named
+    vector1 and vector2, respectively.
+
+    Add 10 and 20 to vector dynamically using push_back
+    Display the elements in vector1 using the at() method as well as its size using the size() method
+    
+    Add 100 and 200 to vector2 dynamically using push_back
+    Display the elements in vector2 using the at() method as well as its size using the size() method
+    
+    Declare an empty 2D vector_2D called vector_2d
+    Remember, that a 2D vector os a vector of vector<int>
+    
+    Add vector1 to vector_2d dynamically using push_back
+    Add vector2 to vector_2d dynamically using push_back
+    
+    Displat the elements in vector_2d again using th at() method
+    
+    Display the elements in vector1
+    
+    What did you expect? did you get what you expected? What do you think will happen?
+    
+    */
