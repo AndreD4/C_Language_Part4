7b3c110ae120976a43bc23dc213b3b6ed140d1f2
AndreD4 <andremai629@gmail.com>
2023-10-05 15:44:15 +0900

    vetcor_2D

--- Vectors/main.cpp
+++ Vectors/main.cpp
@@ -67,15 +67,28 @@ int main()
     
 //    cout << "\nThere are now " << test_scores.size() << " scores in the vertor" << endl;
     
+//    Example of 2D-vectors
     
+    vector <vector<int>> movie_ratings
+    {
+        
+        {1, 2, 3, 4},
+        {1, 2, 4, 4},
+        {1, 3, 4, 5}
+        
+    };
     
+    cout << "\nHere are the movie ratings for reviewer #1 using array syntax: " << endl;
+    cout << movie_ratings [0][0] << endl;
+    cout << movie_ratings [0][1] << endl;
+    cout << movie_ratings [0][2] << endl;
+    cout << movie_ratings [0][3] << endl;
     
-    
-    
-    
-    
-    
-    
+    cout << "\nHere are the movie ratings for reviewer #1 using vector syntax: " << endl;
+    cout << movie_ratings.at(0).at(0) << endl;
+    cout << movie_ratings.at(0).at(1) << endl;
+    cout << movie_ratings.at(0).at(2) << endl;
+    cout << movie_ratings.at(0).at(3) << endl;
     
     cout << endl;
     return 0;    
